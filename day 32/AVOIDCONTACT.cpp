558fdca4213772a6d1d4201bde0ba56b1b9fc3fb
gourikasoni <138545177+gourikasoni@users.noreply.github.com>
2023-09-05 21:50:13 +0530

    Add files via upload

---
+++ day 32/AVOIDCONTACT.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int x, y, a;
+	    cin>>x>>y;
+	    
+	    a = 2*y + (x-y);
+	    
+	    cout<< ( x == y ?  a-1 : a) <<endl;
+	}
+	return 0;
+}
